4e7eb4cc6cecd1bbbbecf9146e4b66fda19de6a4
Elias Hanna <h.elias@hotmail.fr>
2023-03-28 16:10:35 +0200

    upd

--- gym_fastsim/__init__.py
+++ gym_fastsim/__init__.py
@@ -5,6 +5,7 @@ from os.path import dirname, join
 default_env = "assets/LS_maze_hard.xml"
 empty_env = "assets/empty_map.xml"
 huge_empty_env = "assets/empty_map_huge.xml"
+very_huge_empty_env = "assets/empty_map_very_huge.xml"
 
 # logger = logging.getLogger(__name__)
 
@@ -36,4 +37,11 @@ register(
     id='FastsimHugeEmptyMapNavigationPos-v0',
     entry_point='gym_fastsim.simple_nav_pos:SimpleNavPosEnv',
     kwargs={"xml_env":join(dirname(__file__), huge_empty_env)}
+)
+
+
+register(
+    id='FastsimVeryHugeEmptyMapNavigationPos-v0',
+    entry_point='gym_fastsim.simple_nav_pos:SimpleNavPosEnv',
+    kwargs={"xml_env":join(dirname(__file__), very_huge_empty_env)}
 )
