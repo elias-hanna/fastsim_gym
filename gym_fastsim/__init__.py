c5cabd420b082c1899abcf0807d4950a0885177e
Elias Hanna <h.elias@hotmail.fr>
2023-03-28 10:53:07 +0200

    added huge empty map

--- gym_fastsim/__init__.py
+++ gym_fastsim/__init__.py
@@ -1,11 +1,12 @@
-import logging
+# import logging
 from gym.envs.registration import register
 from os.path import dirname, join
 
 default_env = "assets/LS_maze_hard.xml"
 empty_env = "assets/empty_map.xml"
+huge_empty_env = "assets/empty_map_huge.xml"
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
 register(
     id='FastsimSimpleNavigation-v0',
@@ -29,4 +30,10 @@ register(
     id='FastsimEmptyMapNavigationPos-v0',
     entry_point='gym_fastsim.simple_nav_pos:SimpleNavPosEnv',
     kwargs={"xml_env":join(dirname(__file__), empty_env)}
+)
+
+register(
+    id='FastsimHugeEmptyMapNavigationPos-v0',
+    entry_point='gym_fastsim.simple_nav_pos:SimpleNavPosEnv',
+    kwargs={"xml_env":join(dirname(__file__), huge_empty_env)}
 )
