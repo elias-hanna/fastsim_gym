2af29ebcb697fe1c513914a1993f884a89b23058
Elias Hanna <h.elias@hotmail.fr>
2023-06-15 16:59:24 +0200

    added dot and dot2 mazes

--- gym_fastsim/__init__.py
+++ gym_fastsim/__init__.py
@@ -6,6 +6,8 @@ default_env = "assets/LS_maze_hard.xml"
 empty_env = "assets/empty_map.xml"
 huge_empty_env = "assets/empty_map_huge.xml"
 very_huge_empty_env = "assets/empty_map_very_huge.xml"
+dot_env = "assets/dot_maze.xml"
+dot2_env = "assets/dot2_maze.xml"
 
 # logger = logging.getLogger(__name__)
 
@@ -39,9 +41,20 @@ register(
     kwargs={"xml_env":join(dirname(__file__), huge_empty_env)}
 )
 
-
 register(
     id='FastsimVeryHugeEmptyMapNavigationPos-v0',
     entry_point='gym_fastsim.simple_nav_pos:SimpleNavPosEnv',
     kwargs={"xml_env":join(dirname(__file__), very_huge_empty_env)}
+)
+
+register(
+    id='FastsimDotNavigationPos-v0',
+    entry_point='gym_fastsim.simple_nav_pos:SimpleNavPosEnv',
+    kwargs={"xml_env":join(dirname(__file__), dot_env)}
+)
+
+register(
+    id='FastsimDot2NavigationPos-v0',
+    entry_point='gym_fastsim.simple_nav_pos:SimpleNavPosEnv',
+    kwargs={"xml_env":join(dirname(__file__), dot2_env)}
 )
